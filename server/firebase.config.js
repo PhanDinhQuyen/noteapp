81f5cd3d3eb3ee555864c9264ddcd6f51e3015d9
LouisPhan <phanquyen7799@gmail.com>
2023-02-15 15:51:18 +0700

    -server updated

---
+++ server/firebase.config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase-admin/app";
+import dotenv from "dotenv";
+dotenv.config();
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.VITE_apiKey,
+  authDomain: process.env.VITE_authDomain,
+  projectId: process.env.VITE_projectId,
+  storageBucket: process.env.VITE_storageBucket,
+  messagingSenderId: process.env.VITE_messagingSenderId,
+  appId: process.env.VITE_appId,
+  measurementId: process.env.VITE_measurementId,
+};
+
+initializeApp(firebaseConfig);
